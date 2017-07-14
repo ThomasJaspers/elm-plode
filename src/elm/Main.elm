6f350f0c00a6dd8ba75aa9b775174d1920fc057f
Thomas Jaspers <thomas.jaspers@codecentric.de>
2017-07-14 14:07:07 +0200

    Started with model and view

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,10 +2,10 @@ module Main exposing (..)
 
 -- component import example
 
-import Components.Hello exposing (hello)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Types as Types exposing (..)
 
 
 -- APP
@@ -56,33 +56,11 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-        [ -- inline CSS (literal)
-          div [ class "row" ]
-            [ div [ class "col-xs-12" ]
-                [ div [ class "jumbotron" ]
-                    [ img [ src "static/img/elm.jpg", style styles.img ] [] -- inline CSS (via var)
-                    , hello model -- ext 'hello' component (takes 'model' as arg)
-                    , p [] [ text "Elm Webpack Starter" ]
-                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
-                        [ -- click handler
-                          span [ class "glyphicon glyphicon-star" ] [] -- glyphicon
-                        , span [] [ text "FTW!" ]
-                        ]
-                    ]
+    div
+        [ class "jumbotron" ]
+        [ div [ class "container" ]
+            [ div [ class "row" ]
+                [ h1 [] [ text "Elm Unit Converter" ]
                 ]
             ]
         ]
-
-
-
--- CSS STYLES
-
-
-styles : { img : List ( String, String ) }
-styles =
-    { img =
-        [ ( "width", "33%" )
-        , ( "border", "4px solid #337AB7" )
-        ]
-    }
